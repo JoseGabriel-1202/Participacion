ef67d7d9e5f5018e1d373c29e44035b15e7545b6
JoseGabriel-1202 <78770273+JoseGabriel-1202@users.noreply.github.com>
2021-03-09 00:58:56 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace practica_en_clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] numero = { 10, 20, 30, 40, 50};
+            int suma;
+            suma = numero[0] + numero[1] + numero[2] + numero[3] + numero[4];
+            Console.WriteLine("suma valores: {0}", suma);
+            
+        }
+    }
+}
